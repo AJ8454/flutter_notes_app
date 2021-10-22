e6f27568fc1248a1a6010efa7ca804f6fd011853
AJ8454 <ajaypangare8454@gmail.com>
2021-10-22 20:11:47 +0530

    first commit

---
+++ lib/model/note.dart
@@ -0,0 +1,75 @@
+// table name.
+const String tableNotes = 'notes';
+
+// attributes.
+class NoteFields {
+  static final List<String> values = [
+    id,
+    isImportant,
+    number,
+    title,
+    description,
+    time,
+  ];
+
+  static const String id = '_id';
+  static const String isImportant = 'isImportant';
+  static const String number = 'number';
+  static const String title = 'title';
+  static const String description = 'description';
+  static const String time = 'time';
+}
+
+// models
+class Note {
+  final int? id;
+  final bool isImportant;
+  final int number;
+  final String title;
+  final String description;
+  final DateTime createdTime;
+
+  Note({
+    this.id,
+    required this.isImportant,
+    required this.number,
+    required this.title,
+    required this.description,
+    required this.createdTime,
+  });
+
+  Note copy({
+    int? id,
+    bool? isImportant,
+    int? number,
+    String? title,
+    String? description,
+    DateTime? createdTime,
+  }) =>
+      Note(
+        id: id ?? this.id,
+        isImportant: isImportant ?? this.isImportant,
+        number: number ?? this.number,
+        title: title ?? this.title,
+        description: description ?? this.description,
+        createdTime: createdTime ?? this.createdTime,
+      );
+
+  Map<String, Object?> toJson() => {
+        NoteFields.id: id,
+        NoteFields.title: title,
+        NoteFields.isImportant: isImportant ? 1 : 0,
+        NoteFields.number: number,
+        NoteFields.description: description,
+        NoteFields.time: createdTime.toIso8601String(),
+      };
+
+  static Note fromJson(Map<String, Object?> json) => Note(
+        id: json[NoteFields.id] as int?,
+        number: json[NoteFields.number] as int,
+        title: json[NoteFields.title] as String,
+        isImportant: json[NoteFields.isImportant] == 1,
+        description: json[NoteFields.description] as String,
+        createdTime: DateTime.parse(json[NoteFields.time] as String),
+      );
+}
